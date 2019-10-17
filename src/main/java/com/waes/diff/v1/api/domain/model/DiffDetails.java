be7a529de05d28e72aea1370d2830e93cec72c5d
John Silva <aoshibr@gmail.com>
2019-10-17 18:02:59 -0300

    Models for Details, Payload data and Diff Result

---
+++ src/main/java/com/waes/diff/v1/api/domain/model/DiffDetails.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2015-2019 John Silva.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.waes.diff.v1.api.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.validation.annotation.Validated;
+
+@Data @AllArgsConstructor @ApiModel(description = "Holds the length and offset of differences") @Validated public class DiffDetails {
+
+	@JsonProperty("offset") @ApiModelProperty("offset") private Integer offset;
+
+	@JsonProperty("length") @ApiModelProperty("length") private Integer length;
+}
---
+++ src/main/java/com/waes/diff/v1/api/domain/model/PayloadDiffResult.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2015-2019 John Silva.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.waes.diff.v1.api.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.waes.diff.v1.api.domain.enums.Result;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.Valid;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Holds the comparison result - EQUAL - NOT_EQUAL - SIZE_NOT_MATCH
+ */
+@ApiModel(description = "Holds the comparison result   - EQUAL  - NOT_EQUAL  - SIZE_NOT_MATCH  ") @Validated @Data public class PayloadDiffResult {
+	@JsonProperty("result") @ApiModelProperty("result") private Result result;
+
+	@Valid @JsonProperty("details") @ApiModelProperty("details") private List<DiffDetails> details;
+
+	private PayloadDiffResult() {
+		details = Optional.ofNullable(details).orElse(new LinkedList<>());
+	}
+
+	public static PayloadDiffResult create() {
+		return new PayloadDiffResult();
+	}
+
+	/**
+	 * Payload's result data
+	 *
+	 * @param result the diff result
+	 * @return current object instance
+	 */
+	public PayloadDiffResult result(Result result) {
+		this.result = result;
+		return this;
+	}
+
+	/**
+	 * Adds a list of details
+	 *
+	 * @param details the payload diff result details
+	 * @return current object instance
+	 */
+	public PayloadDiffResult addDetails(List<DiffDetails> details) {
+		this.details.addAll(details);
+		return this;
+	}
+}
