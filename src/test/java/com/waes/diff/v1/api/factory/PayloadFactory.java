7993c486313a37fc32085fd8fcd5ef64e3b40eb2
John Silva <aoshibr@gmail.com>
2019-10-18 10:20:28 -0300

    PayloadFactory created and added tests to PayloadController

---
+++ src/test/java/com/waes/diff/v1/api/factory/PayloadFactory.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2015-2019 John Silva.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.waes.diff.v1.api.factory;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.waes.diff.v1.api.domain.enums.Direction;
+import com.waes.diff.v1.api.repository.entity.Payload;
+import org.assertj.core.util.Lists;
+
+import java.util.List;
+
+import static com.waes.diff.v1.api.repository.entity.Payload.create;
+
+public class PayloadFactory {
+
+	public static final String ID_1 = "id_1";
+	public static final String ID_2 = "id_2";
+	public static final String INVALID_PAYLOAD_CONTENT = "{\"content\" : \"\"}";
+	private static final String LEFT_1 = "IHsKICAgIm5hbWUiOiJXYWVzIiwKICAgImNvdW50cnkiOiJOZXRoZXJsYW5kcyIKIH0K";
+	private static final String RIGHT_1 = "IHsKICAgIm5hbWUiOiJXYWVzIiwKICAgImNvdW50cnkiOiJOZXRoZXJsYW5kcyIKIH0K";
+	private static final String LEFT_2 = "IHsKICAgIm5hbWUiOiIxQnl0ZSIsCiAgICJjb3VudHJ5IjoiQnJhemlsIgogfQo=";
+	private static final String RIGHT_2 = "IHsKICAgIm5hbWUiOiIxQnl0ZSIsCiAgICJjb3VudHJ5IjoiQnJhemlsIgogfQo=";
+	private static final String LEFT_3 = "Y29tcGFyYXRpb24gc2l6ZQ==";
+	private static final String RIGHT_3 = "a29tcGFyYXRpb24gc2l0ZQ==";
+
+	private static final String CREATED_MESSAGE = "Payload [id:%s] created";
+
+	public static Payload leftPayload1() {
+		return Payload.create().content(LEFT_1).direction(Direction.LEFT).id(ID_1);
+	}
+
+	public static Payload rightPayload1() {
+		return create().content(RIGHT_1).direction(Direction.RIGHT).id(ID_1);
+	}
+
+	public static Payload leftPayload2() {
+		return create().content(LEFT_2).direction(Direction.LEFT).id(ID_2);
+	}
+
+	private static Payload rightPayload2() {
+		return create().content(RIGHT_2).direction(Direction.RIGHT).id(ID_2);
+	}
+
+	private static Payload leftPayload3() {
+		return create().content(LEFT_3).direction(Direction.LEFT).id(ID_2);
+	}
+
+	private static Payload rightPayload3() {
+		return create().content(RIGHT_3).direction(Direction.RIGHT).id(ID_2);
+	}
+
+	public static List<Payload> listOfPayload() {
+		return Lists.newArrayList(leftPayload1(), rightPayload1());
+	}
+
+	public static List<Payload> sameSizeNotEqualListOfPayload() {
+		return Lists.newArrayList(leftPayload3(), rightPayload3());
+	}
+
+	public static List<Payload> notEqualListOfPayload() {
+		return Lists.newArrayList(leftPayload1(), rightPayload2());
+	}
+
+	public static String payloadContent(Payload payload) throws JsonProcessingException {
+		return new ObjectMapper().writeValueAsString(payload);
+	}
+
+	public static String createdMessage(String uid) {
+		return String.format(CREATED_MESSAGE, uid);
+	}
+}
--- src/test/java/com/waes/diff/v1/api/resource/PayloadControllerTest.java
+++ src/test/java/com/waes/diff/v1/api/resource/PayloadControllerTest.java
@@ -18,7 +18,6 @@ package com.waes.diff.v1.api.resource;
 import com.waes.diff.v1.api.repository.entity.Payload;
 import com.waes.diff.v1.api.service.PayloadService;
 import org.hamcrest.core.Is;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -26,50 +25,42 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static com.waes.diff.v1.api.domain.response.PayloadResponse.create;
+import static com.waes.diff.v1.api.factory.PayloadFactory.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(PayloadController.class) class PayloadControllerTest {
 
-  final String leftPayload_1 = "{\"content\":\"Y29tcGFyYXRpb24gc2l6ZQ==\"}";
-  final String rightPayload_1 = "{\"content\":\"Y29tcGFyYXRpb24gc2l6ZQ==\"}";
-  final String leftPayload_2 = "{\"content\":\"a29tcGFyYXRpb24gc2l0ZQ==\"}";
-  final String rightPayload_2 = "{\"content\":\"a29tcGFyYXRpb24gc2l0ZQ==\"}";
-  final String invalidPayload = "{\"content\":\"\"}";
-
   @Autowired MockMvc mockMvc;
   @MockBean PayloadService payloadService;
 
-  @BeforeEach void setUp() {
-  }
-
   @Test void addLeftPayload_whenValidData_AddToCollection() throws Exception {
-    given(payloadService.save(any(Payload.class))).willReturn(create().message("CREATED"));
-    mockMvc.perform(post("/v1/diff/{id}/left", "uid").contentType(APPLICATION_JSON).content(leftPayload_1))
-            .andExpect(status().isCreated()).andExpect(jsonPath("$.message", Is.is("CREATED")))
-            .andExpect(content().contentType(APPLICATION_JSON));
+    given(payloadService.save(any(Payload.class))).willReturn(create().message(createdMessage(ID_1)));
+    mockMvc.perform(post("/v1/diff/{id}/left", ID_1).contentType(APPLICATION_JSON_VALUE).content(payloadContent(leftPayload1())))
+            .andExpect(status().isCreated()).andExpect(jsonPath("$.message", Is.is(createdMessage(ID_1))))
+            .andExpect(content().contentType(APPLICATION_JSON_VALUE));
     verify(payloadService).save(any(Payload.class));
   }
 
   @Test void addRightPayload_whenValidData_AddToCollection() throws Exception {
-    given(payloadService.save(any(Payload.class))).willReturn(create().message("CREATED"));
-    mockMvc.perform(post("/v1/diff/{id}/right", "uid").content(rightPayload_1).contentType(APPLICATION_JSON))
-            .andExpect(status().isCreated()).andExpect(jsonPath("$.message", Is.is("CREATED")))
-            .andExpect(content().contentType(APPLICATION_JSON));
+    given(payloadService.save(any(Payload.class))).willReturn(create().message(createdMessage(ID_2)));
+    mockMvc.perform(post("/v1/diff/{id}/right", ID_2).contentType(APPLICATION_JSON_VALUE).content(payloadContent(rightPayload1())))
+            .andExpect(status().isCreated()).andExpect(jsonPath("$.message", Is.is(createdMessage(ID_2))))
+            .andExpect(content().contentType(APPLICATION_JSON_VALUE));
     verify(payloadService).save(any(Payload.class));
   }
 
   @Test void whenAddLeftPayloadWithInvalidBody_returnNotFound() throws Exception {
-    mockMvc.perform(post("/v1/diff/{id}/left", "udi").content(invalidPayload).contentType(APPLICATION_JSON))
+    mockMvc.perform(post("/v1/diff/{id}/left", ID_1).content(INVALID_PAYLOAD_CONTENT).contentType(APPLICATION_JSON_VALUE))
             .andExpect(status().isBadRequest()).andExpect(jsonPath("$.content", Is.is("Content should not be null or empty")));
   }
 
   @Test void whenAddRightPayloadWithInvalidBody_returnNotFound() throws Exception {
-    mockMvc.perform(post("/v1/diff/{id}/right", "udi").content(invalidPayload).contentType(APPLICATION_JSON))
+    mockMvc.perform(post("/v1/diff/{id}/right", ID_2).content(INVALID_PAYLOAD_CONTENT).contentType(APPLICATION_JSON_VALUE))
             .andExpect(status().isBadRequest()).andExpect(jsonPath("$.content", Is.is("Content should not be null or empty")));
   }
 }
