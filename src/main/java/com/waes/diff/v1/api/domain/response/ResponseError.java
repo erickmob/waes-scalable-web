9d9d5260d2b4fe41ca03c93dfb2101394e92e940
John Silva <aoshibr@gmail.com>
2019-10-21 00:58:39 -0300

    Refactoring path

--- src/main/java/com/waes/diff/v1/api/domain/response/ResponseError.java
+++ src/main/java/com/waes/diff/v1/api/domain/response/ResponseError.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */ 
-package com.waes.diff.v1.api.domain.exception.error;
+package com.waes.diff.v1.api.domain.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
